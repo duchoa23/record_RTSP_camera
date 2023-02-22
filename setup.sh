feff93786b1eb36220e33fb22723a93f45efbba5
duchoa23 <56620050+duchoa23@users.noreply.github.com>
2023-02-22 08:49:17 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#cài đặt các pack: ntp, epel, unzip, wget, ffmpeg, rclone
+#cài đặt các package: ntp, epel, unzip, wget, ffmpeg, rclone
 sudo yum install -y ntpdate
 sudo ln -f -s /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime
 sudo ntpdate vn.pool.ntp.org
@@ -9,16 +9,18 @@ sudo yum -y install wget
 sudo rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
 sudo yum install ffmpeg ffmpeg-devel -y
 curl https://rclone.org/install.sh | sudo bash
-#tạo thư mục và chmod
+#tạo thư mục và chmod, ở đây tạo thư mục record để lưu file setup và tạo thư mục camera để lưu file video
 sudo mkdir /etc/record
 sudo mkdir /etc/record/camera
 sudo chmod -R 777 /etc/record
 sudo chmod -R 777 /etc/record/camera
+#Tải file cấu hình
 sudo wget --no-check-certificate -P /etc/record/ "https://toantinh.net/tools/setup.zip"
 sudo unzip -d /etc/record /etc/record/setup.zip
 sudo unzip /etc/record/setup.zip
 sudo chmod -R 777 /etc/record/record.sh
 sudo chmod -R 777 /etc/record/rclone.sh
+#Tạo crontab
 cron_job="0 * * * * /etc/record/record.sh"
 cron_job2="0 0 * * * /etc/record/rclone.sh"
 echo "$cron_job" >> /tmp/new_cron_job
