b5ad0c9417ac739b854c6c26611c947349960826
duchoa23 <56620050+duchoa23@users.noreply.github.com>
2023-02-22 09:32:28 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ sudo yum install -y ntpdate
 sudo ln -f -s /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime
 sudo ntpdate vn.pool.ntp.org
 sudo yum -y install epel-release
+sudo yum -y install unzip
 sudo yum -y install wget
 sudo rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
 sudo yum install ffmpeg ffmpeg-devel -y
