815bf93493bfe8372815b2afc243ead1bc9d74b8
duchoa23 <56620050+duchoa23@users.noreply.github.com>
2023-02-22 09:12:12 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ sudo yum install -y ntpdate
 sudo ln -f -s /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime
 sudo ntpdate vn.pool.ntp.org
 sudo yum -y install epel-release
-sudo yum -y install unzip
 sudo yum -y install wget
 sudo rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
 sudo yum install ffmpeg ffmpeg-devel -y
@@ -15,9 +14,8 @@ sudo mkdir /etc/record/camera
 sudo chmod -R 777 /etc/record
 sudo chmod -R 777 /etc/record/camera
 #Tải file cấu hình
-sudo wget --no-check-certificate -P /etc/record/ "https://toantinh.net/tools/setup.zip"
-sudo unzip -d /etc/record /etc/record/setup.zip
-sudo unzip /etc/record/setup.zip
+sudo wget --no-check-certificate -P /etc/record/ "https://raw.githubusercontent.com/duchoa23/record_RTSP_camera/main/record/rclone.sh"
+sudo wget --no-check-certificate -P /etc/record/ "https://raw.githubusercontent.com/duchoa23/record_RTSP_camera/main/record/record.sh"
 sudo chmod -R 777 /etc/record/record.sh
 sudo chmod -R 777 /etc/record/rclone.sh
 #Tạo crontab
